a014aa54cb50891075147b7fd372c7347efaf88f
syryuauros <sy.ryu@aurostech.com>
2022-07-06 17:23:42 +0900

    Doom emacs installed

--- flake.nix
+++ flake.nix
@@ -6,13 +6,34 @@
       url = "github:nix-community/home-manager/release-22.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    doom-private.url = "github:syryuauros/doom-private";
+    doom-private.flake = false;
+    nix-doom-emacs.url = "github:syryuauros/nix-doom-emacs";
+    nix-doom-emacs.inputs.doom-private.follows = "doom-private";
   };
 
   outputs = inputs: {
-    nixosConfigurations = {
-      syryuhds = inputs.nixpkgs.lib.nixosSystem {
+    nixosConfigurations =
+      let
         system = "x86_64-linux";
+      in
+      {
+      syryuhds = inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
         modules = [
+          ({
+             nixpkgs = {
+               inherit system;
+               overlays = [ inputs.nix-doom-emacs.overlay ];
+             };
+          })
+          ({pkgs, ...} :{
+            services.emacs = {
+              enable = true;
+              package = pkgs.doom-emacs;
+              # client.enable = true;
+            };
+          })
           (import ./configuration.nix)
         ];
       };
