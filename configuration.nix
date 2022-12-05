9039c873329af903dbbef02ad9cf32cf1f49b6dc
syryuauros <sy.ryu@aurostech.com>
2022-12-05 09:40:16 +0900

    cache server changed

--- configuration.nix
+++ configuration.nix
@@ -47,14 +47,16 @@
     #trustedUsers = [ "root" "@admin" "@wheel" ];
     binaryCaches = [
       "https://cache.nixos.org/"
-      "https://hydra.iohk.io"
+      #"https://hydra.iohk.io"
+      "https://hydra.snabb.co"    #https://nix.dev/faq
       "https://cachix.cachix.org"
       "https://nix-community.cachix.org"
       "http://haedosa.xyz:2006"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      #"hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      "hydra.snabb.co-1:zPzKSJ1mynGtYEVbUR0QVZf9TLcaygz/OyzHlWo5AMM="
       "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "builder6:LoWfwaMHhw0E4FrXq3qlTvslOgZHh7fIPFVcfPy3UXo="
