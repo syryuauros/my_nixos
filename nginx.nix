cdf4590162d3d3e462649a30bb23fea94f7aa2d4
syryuauros <sy.ryu@aurostech.com>
2023-03-21 12:03:53 +0900

    nginx setup for exposing shapemaster-revealjs

--- nginx.nix
+++ nginx.nix
@@ -2,37 +2,32 @@
 
   services.nginx = {
     enable = true;
+
     virtualHosts."_"= {
       listen = [ { addr = "0.0.0.0"; port = 80; ssl = false; } ];
       locations."/" = {
-        proxyPass = "http://localhost:35901";
+         proxyPass = "http://localhost:35901";
+        #root = "/home/auros/gits/shapemaster/daily/test2.html";
         #proxyWebsockets = true;
-      };
+        };
       locations."/ws/" = {
         proxyPass = "http://localhost:35903";
         proxyWebsockets = true;
+        };
       };
 
+    virtualHosts."__"= {
+      listen = [ {addr = "0.0.0.0"; port = 50696; ssl = false; } ];
+      #root = "/home/auros/gits/shapemaster/daily";
+      locations."/" = {
+        root = /home/auros/gits/shapemaster/daily;
+        extraConfig =
+          "autoindex on;"
+          ;
+        };
+      };
 
-      # locations."/ws" = {
-      #   proxyPass = "http://0.0.0.0:35903";
-      #   proxyWebsockets = true;
-      # };
     };
-
-    # virtualHosts."192.168.12.135"= {
-    #  listen = [ { addr = "0.0.0.0"; port = 80; ssl = false; } ];
-    #   locations."/ws" = {
-    #     proxyPass = "http://localhost:35903";
-    #     proxyWebsockets = true;
-    #   };
-    #  # locations."/" = {
-    #  #   proxyPass = "http://localhost:35903";
-    #  #   proxyWebsockets = true;
-    #  # };
-    # };
-
-  };
-}
+  }
 
 # http://brownbears.tistory.com/191 -- reverse proxy server concepts
