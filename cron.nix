89b96d3b97cb4d69d0006ec996721a5414022c9d
syryuauros <sy.ryu@aurostech.com>
2023-03-24 19:06:09 +0900

    update

--- cron.nix
+++ cron.nix
@@ -4,9 +4,9 @@
 
   services.cron = {
     enable = true;
-    systemCronJobs = [
-      "*/5 * * * *  auros  [ ! `git fetch > /dev/null 2>&1 && git pull | grep file | wc -l` = 0 ] && sudo nixos-rebuild switch --flake ~/gits/my_nixos#syryuhds --impur"
-      ];
+    systemCronJobs = [''
+      */5 * * * *     auros      git --git-dir=/home/auros/gits/shapemaster/.git --work-tree=/home/auros/gits/shapemaster pull
+      ''];
     };
   }
 
