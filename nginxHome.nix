c2849f022a22bac2527c517b74b53eb904d2ade6
syryuauros <sy.ryu@aurostech.com>
2023-05-25 12:09:22 +0900

    update

---
+++ nginxHome.nix
@@ -0,0 +1,9 @@
+{
+  home = {
+    shellAliases = {
+      nginx_confirm_config="ls /nix/store/ | grep nginx.conf | xargs -I {} stat /nix/store/{} | grep -E 'File|Birth'";
+      nginx_update="rsync -av --delete /home/auros/gits/shapemaster/daily/ /var/www/SM_daily/ | rsync -av --delete /home/auros/gits/programming/Haskell/projects/miso/result/bin/app.jsexe/ /var/www/miso/";
+      nginx_authority_setup="sudo chown -R auros:users /var/www";
+    };
+  };
+}
