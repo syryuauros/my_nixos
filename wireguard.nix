2dd56c66bb9d49392ac1f4356a1d86a631f17baf
syryuauros <sy.ryu@aurostech.com>
2023-09-11 14:13:51 +0900

    wireguard correction 1

--- wireguard.nix
+++ wireguard.nix
@@ -1,25 +1,31 @@
 { config, lib, pkgs, ... }: {
 
-  networking.nat ={
-    enable = true;
-    externalInterface = "enp1s0";
-    internalInterface = "sra0";
+  networking ={
+    nat.enable = true;
+    nat.externalInterface = "eth0";
+    nat.internalInterface = [ "sra0" ];
+    firewall = {
+      allowedUDPPorts = [ 51820 ];
+    };
   };
   networking.wireguard.interfaces = {
     sra0 = {
-
-
       ips = [ "10.100.0.1/24"];
       listenPosrt = 51820;
       postSetup = ''
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
       '';
       postShutdown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o enp1s0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
       '';
       privateKeyFile = "~/wireguard-keys/private/";
 
       peers = [
+        {
+          publicKey = "kNteCelNfQlGjhhWcP/OtE9Rc12bdPMgpDsmzx7kpn8=";
+          allowedIPs = [ "10.100.0.3/32" ];
+        }
+
         {
           publicKey = "Uzld/a3SscjfNlLKIre+wuesxt1dyk3pJN6SqZJZ1XI=";
           allowedIPs = [ "10.100.0.2/32" ];
@@ -29,3 +35,6 @@
   };
 
   }
+
+#https://nixos.wiki/wiki/WireGuard
+#https://github.com/jjdosa/mysystem/blob/a8d661be2c7c9a739211f772de574a8e5138dd6d/nixosConfigurations/features/wireguard.nix
