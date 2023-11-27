bc312eb054019b60ea3ff9bb75661f924155943b
syryuauros <sy.ryu@aurostech.com>
2023-11-27 16:40:19 +0900

    add ripgrep for system environment program

--- configuration.nix
+++ configuration.nix
@@ -168,6 +168,7 @@
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
       git
+      ripgrep
      #gnuplot
       brave
       tmux
